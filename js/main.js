1c4da23deb9ef5c95142cb5ad57481d48296c621
robs-am <roberta.amaro89@gmail.com>
2022-05-19 08:19:18 -0300

    add link na logo

--- js/main.js
+++ js/main.js
@@ -1,16 +1,8 @@
-/* function onScroll() {
-    if (scrollY > 0) {
-    navigation.classList.add('scroll')
-    } else {
-        navigation.classList.remove('scroll')
-    }
-
-} */
 
 function onScroll() {
     if (scrollY > 0) {
         navigation.classlist.add('scroll')
     } else {
-        navigation.classlist.add('remove')
+        navigation.classlist.remove('scroll')
     }
 } 
