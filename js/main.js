62ef94920a987cc3a6ab07649208562d9102d556
robs-am <roberta.amaro89@gmail.com>
2022-05-18 23:11:01 -0300

    add reposicionamento flex

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-function onScroll() {
+/* function onScroll() {
     if (scrollY > 0) {
     navigation.classList.add('scroll')
     } else {
         navigation.classList.remove('scroll')
     }
 
-}
+} */
